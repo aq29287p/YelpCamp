a7655e4aee05663318363cdf8dc54ed2f063254d
aq29287p <a13579230@gmail.com>
2022-10-10 15:13:50 +0800

    server error validate

--- Final_project/app.js
+++ Final_project/app.js
@@ -1,18 +1,20 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
 const path = require('path');
-const methodOverride = require('method-override');
+const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const { campgroundSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
-const port = 3000;
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => { console.log("Database connected"); });
+
+const app = express();
+
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -20,40 +22,54 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+const validateCampground = (req, res, next) => {
+
+    const { error } = campgroundSchema.validate(req.body);
+
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+
+
+
 app.get('/', (req, res) => res.render('home'));
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
     const camp = await Campground.find({});
     res.render('campgrounds/index', { camp });
-});
-app.get('/campgrounds/new', catchAsync((req, res) => {
-    res.render('campgrounds/new');
 }));
-app.post('/campgrounds', catchAsync(async (req, res) => {
-    if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
+
     const camp = new Campground(req.body.campground);
     await camp.save();
     res.redirect(`/campgrounds/${camp._id}`);
 }));
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const camp = await Campground.findById(id);
+    const camp = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { camp });
 }));
 app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const camp = await Campground.findById(id);
+
+    const camp = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', { camp });
 }));
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
-    const camp = await Campground.findByIdAndUpdate(id, { ...req.body.camp });
+    const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${camp._id}`);
 }));
 
 app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const camp = await Campground.findByIdAndDelete(id);
+
+    const camp = await Campground.findByIdAndDelete(req.params.id);
     res.redirect('/campgrounds');
 }));
 app.all('*', (req, res, next) => {
@@ -61,12 +77,12 @@ app.all('*', (req, res, next) => {
 });
 //error handler
 app.use((err, req, res, next) => {
-    const { statusCode = 500, message = 'Something went wrong' } = err;
+    const { statusCode = 500 } = err;
     if (!err.message) err.message = 'Oh no, something went wrong!';
     res.status(statusCode).render('error', { err });
 });
 
-
+const port = 3000;
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 });
